ca789b69d1ee687097ca5a4c97246775547a5afe
Robert Vera <robertvera@RobertVra05Mini.attlocal.net>
2018-09-17 17:32:15 -0500

    Added main function and doesUrlExist function. Added input variables for location and dates.

--- flights.py
+++ flights.py
@@ -1,4 +1,6 @@
 #imports for python script
+import os
+import sys
 import http.client
 
 #function for checking if url exists
@@ -10,8 +12,17 @@ def doesUrlExist(url):
     else:
         print("Website does not exist")
 
-#main function for flights python script
-def __main__():
+#main function
+def main():
+    location = input('Enter your destination: ')
+    initialDate = input('Enter the initial date:')
+    if initialDate < date.today():
+        print('Initial date cannot be before todays date')
+    endDate = input('Enter the end date:')
+    print('Checking for cheapest flights between dates ', initialDate,' and ', endDate, ' to destination ', location)
     url = "https://www.google.com/flights#flt=/m/0f2rq..2018-10-02*./m/0f2rq.2018-10-06;c:USD;e:1;ls:1w;sd:0;t:h"
     doesUrlExist(url)
+
+if __name__ == "__main__":
+    main()
 
