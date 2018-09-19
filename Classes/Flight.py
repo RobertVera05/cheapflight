0b611a200b0f1b52b4223fab11626fbd05b19708
Robert Vera <robert.vera05@Roberts-MBP.attlocal.net>
2018-09-18 22:35:20 -0500

    Created init function for Flight object, also created setters and getters for the class Flight.

--- Classes/Flight.py
+++ Classes/Flight.py
@@ -1,3 +1,24 @@
 class Flight:
     def __init__(location, initialDate, returnDate):
+        self.location = location
+        self.initialDate = initialDate
+        self.returnDate = returnDate
+
+    def getLocation():
+        return self.location
+
+    def setLocation(location):
+        self.location = location
+    
+    def getInitialDate():
+        return initialDate
+    
+    def setInitialDate(initialDate):
+        self.initialDate = initialDate
+
+    def getReturnDate():
+        return returnDate
+
+    def setReturnDate(returnDate):
+        self.returnDate = returnDate
         
